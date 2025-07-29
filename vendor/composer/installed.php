384a435ae8b44b502a3159b215d413b12c55ab78
badasswp <badasswpdev@gmail.com>
2025-07-29 07:12:08 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/display-site-notification-bar',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cfcdb5307d64c7474f687bb13183f873d5fca1ad',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/display-site-notification-bar' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cfcdb5307d64c7474f687bb13183f873d5fca1ad',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
