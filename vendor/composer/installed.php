d60432b94bf755af29884e6eb250f28783d8ba83
badasswp <badasswpdev@gmail.com>
2026-05-24 16:04:27 +0000

    Build from release-1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/display-site-notification-bar',
+        'pretty_version' => 'dev-release-1.1.0-built',
+        'version' => 'dev-release-1.1.0-built',
+        'reference' => '8131ab577536328d1ed04cdbec9554dd063ab329',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/display-site-notification-bar' => array(
+            'pretty_version' => 'dev-release-1.1.0-built',
+            'version' => 'dev-release-1.1.0-built',
+            'reference' => '8131ab577536328d1ed04cdbec9554dd063ab329',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
