cb5139cbb27f336641c344b9270eff210dd7b326
badasswp <badasswpdev@gmail.com>
2026-06-14 21:04:53 +0000

    Build from release-1.2.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/display-site-notification-bar',
+        'pretty_version' => 'dev-release-1.2.0-built',
+        'version' => 'dev-release-1.2.0-built',
+        'reference' => '39d62015a765407ef48443cfa3c1df88a7b445df',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/display-site-notification-bar' => array(
+            'pretty_version' => 'dev-release-1.2.0-built',
+            'version' => 'dev-release-1.2.0-built',
+            'reference' => '39d62015a765407ef48443cfa3c1df88a7b445df',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
